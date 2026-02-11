392a742801c6a22509a3884b53722b937d76ab6e
sarmin52752 <sarmin52752@gmail.com>
2026-02-12 03:33:54 +0600

    Update 4k.js

--- scripts/cmds/4k.js
+++ scripts/cmds/4k.js
@@ -2,7 +2,7 @@ const a = require('axios');
 const tinyurl = require('tinyurl');
 const baseApiUrl = async () => {
   const base = await a.get(
-    `https://raw.githubusercontent.com/Blankid018/D1PT0/main/baseApiUrl.json`,
+    `https://raw.githubusercontent.com/Mostakim0978/D1PT0/refs/heads/main/baseApiUrl.json`,
   );
   return base.data.api;
 };
